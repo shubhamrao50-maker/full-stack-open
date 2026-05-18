54d36adbdeb75907c477219d6677351ccbde22f5
shubhamrao50-maker <shubhamrao50@gmail.com>
2026-05-18 10:05:19 +0530

    1.2: Course Information, step 2 completed

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,10 @@
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.part} {props.exercises}</p>      
+    </div> 
+  )
+} 
 const Header = (props) => {
   return (
     <h1>{props.course}</h1>
@@ -6,10 +13,10 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
-      </div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </div>
   )
 }
 const Total = (props) =>{
